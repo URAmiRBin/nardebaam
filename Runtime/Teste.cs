c0eb8e55a20d0f1dd70388a36a10f11efc412309
Amir Bin <ur.amirbin@gmail.com>
2022-04-20 18:54:21 +0430

    Add assembly definition

--- Runtime/Teste.cs
+++ Runtime/Teste.cs
@@ -2,17 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Teste : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
+namespace Medrick.Nardeboon {
+    public class Teste : MonoBehaviour
     {
-        
-    }
+        // Start is called before the first frame update
+        void Start()
+        {
+            
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        // Update is called once per frame
+        void Update()
+        {
+            
+        }
     }
 }
