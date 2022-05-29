9dc6e8bf8b961cbbd785a0fcccb43fd2587c1ba2
Amir Bin <ur.amirbin@gmail.com>
2022-05-29 13:39:52 +0430

    Add basic facebook analytics config and implementation

--- Runtime/Analytics/FacebookAnalytics.cs
+++ Runtime/Analytics/FacebookAnalytics.cs
@@ -8,44 +8,20 @@ namespace Medrick.Nardeboon
 {
     public class FacebookAnalytics : AnalyticsSystem
     {
-        protected override void SendLevelStartEvent(int level)
-        {
-            FB.Init();
-        }
+        protected override void SendLevelStartEvent(int level) { }
 
-        protected override void SendLevelWinEvent(int level)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        protected override void SendLevelLoseEvent(int level)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        protected override void SendCustomEvent(string type, float value)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        protected override void SendCurrencySpendEvent(int value)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        protected override void SendCurrencyEarnEvent(int value)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        protected override void SendAdFailEvent()
-        {
-            throw new System.NotImplementedException();
-        }
-
-        protected override void SendAdShowEvent()
-        {
-            throw new System.NotImplementedException();
-        }
+        protected override void SendLevelWinEvent(int level) { }
+        
+        protected override void SendLevelLoseEvent(int level) { }
+        
+        protected override void SendCustomEvent(string type, float value) { }
+        
+        protected override void SendCurrencySpendEvent(int value) { }
+        
+        protected override void SendCurrencyEarnEvent(int value) { }
+        
+        protected override void SendAdFailEvent() { }
+        
+        protected override void SendAdShowEvent() { }
     }
 }
